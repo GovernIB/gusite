b7b780f6ff52f05c0fd036b4984ba0a77b3f30c6
slromero-indra <slromero@indra.es>
2017-10-10 18:20:20 +0200

    #96 Incluido el control de version por bbdd.

---
+++ scripts/bbdd/1.4/gusite_update_data_from_1.4.2_to_1.4.3.sql
@@ -0,0 +1,2 @@
+-- nuevas plantillas para el tipo mapa
+update gus_micros set mic_version = 'v1' where mic_version is null;
---
+++ scripts/bbdd/1.4/gusite_update_schema_from_1.4.2_to_1.4.3.sql
@@ -0,0 +1,2 @@
+--Marca la versión como no nulo.
+  alter table gus_micros modify mic_version not null;
